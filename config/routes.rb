5cc5515e10b7b97dd90aed2b8e497220b744c82e
Junki Ishida <junky5092@gmail.com>
2019-04-01 22:15:44 +0900

    added some styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-  get 'baskets', to: 'baskets#index'
-  get 'baskets/:id', to: 'baskets#show', as: :basket
 
-  post 'baskets', to: 'baskets#create'
+  resources :baskets do
+    resources :about, only: [:index]
+    resources :contact, only: [:index]
+  end
+
+  # get 'about', to: 'pages#about'
+  # get 'contact', to: 'pages#contact'
+  # get 'baskets', to: 'baskets#index'
+  # get 'baskets/:id', to: 'baskets#show', as: :basket
+  # post 'baskets', to: 'baskets#create'
+
 
   # verb path, to: 'controller#action'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

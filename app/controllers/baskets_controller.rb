5660cd9ef57c254676ab4be6142f68eb65cc6e80
Junki Ishida <junky5092@gmail.com>
2019-04-03 23:22:10 +0900

    modified baskets controller

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,18 +1,12 @@
 class BasketsController < ApplicationController
 
-  # Let's fake a database records
-  BASKETS = {
-    1 => { name: "Tabuse", position: "Point Guard", nationality: "Japan" },
-    2 => { name: "Watanabe", position: "Forward", nationality: "Japan" },
-    3 => { name: "Harden", position: "Point Guard", nationality: "USA" }
-  }
+  before_action :set_basket, [:show, :edit, :update, :destroy]
 
   def index
     @baskets = Basket.all
   end
 
   def show
-    @basket = Basket.find(params[:id])
   end
 
   def new
@@ -20,21 +14,39 @@ class BasketsController < ApplicationController
   end
 
   def create
-
+    @basket = Basket.create(basket_params)
+    redirect_to baskets_path
   end
 
   def edit
-    @basket = Basket.find(params[:id])
   end
 
   def update
-
+    @basket = Basket.update(params[:basket])
+    redirect_to baskets_path
   end
 
-  def delete
-    @basket = Basket.find(params[:id])
+  def destroy
     @basket.destroy
+    redirect_to baskets_path
   end
+
+  private
+
+  def basket_params
+    params.require(:basket).permit(:name, :position, :nationality)
+  end
+
+  def set_basket
+    @basket = Basket.find(params[:id])
+  end
+end
+  # # Let's fake a database records
+  # BASKETS = {
+  #   1 => { name: "Tabuse", position: "Point Guard", nationality: "Japan" },
+  #   2 => { name: "Watanabe", position: "Forward", nationality: "Japan" },
+  #   3 => { name: "Harden", position: "Point Guard", nationality: "USA" }
+  # }
 
   # def index
   #   if params[:player_type].blank?
@@ -54,5 +66,3 @@ class BasketsController < ApplicationController
   #   @basket = BASKET.new(name: params[:name], address: params[:address])
   #   @basket.save
   # end
-
-end
